e22914326406c0ed9dcf888ead087658428600c8
Scott Key <scottdkey@gmail.com>
2026-05-08 00:46:37 -0600

    fix: clippy field_reassign_with_default in bash tools test

--- projects/llm/src/tools/bash.rs
+++ projects/llm/src/tools/bash.rs
@@ -151,8 +151,7 @@ mod tests {
 
     #[test]
     fn is_allowed_auto_approve_allows_all() {
-        let mut perms = BashPermissions::default();
-        perms.auto_approve = true;
+        let perms = BashPermissions { auto_approve: true, ..Default::default() };
         assert!(perms.is_allowed("rm -rf /"));
         assert!(perms.is_allowed("any command at all"));
     }
