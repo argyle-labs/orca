89677f2a6da53239aa2a3f163151f432fc71ded2
Scott Key <scottdkey@gmail.com>
2026-05-09 14:24:00 -0600

    wip

--- projects/sdk-go/cmd/conformance-plugin/main.go
+++ projects/sdk-go/cmd/conformance-plugin/main.go
@@ -17,9 +17,9 @@ import (
 	"os"
 	"time"
 
-	"orca/sdk-go/manifest"
-	"orca/sdk-go/pki"
-	"orca/sdk-go/transport"
+	"github.com/scottdkey/orca/projects/sdk-go/manifest"
+	"github.com/scottdkey/orca/projects/sdk-go/pki"
+	"github.com/scottdkey/orca/projects/sdk-go/transport"
 )
 
 const (
